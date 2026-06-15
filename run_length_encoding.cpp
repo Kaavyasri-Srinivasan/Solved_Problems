49c452263fe74d16d7cd277bf108a937573bfbb4
Kaavyasri-Srinivasan <kaavyasri.069@gmail.com>
2026-06-15 12:25:36 +0530

    Add run-length encoding functionality
    Implement run-length encoding for a given string.

---
+++ run_length_encoding.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main(){
+	char string[100];
+	cout<<"Enter the string:";
+	cin>>string;
+	int len=strlen(string);
+	int i=0;
+	int j;
+	int k=0;
+	int count=1;
+	int char_count[10];
+	while(i<len){
+		j=i;
+		while(j<len-1 && string[j]==string[j+1]){
+			count=count+1;
+			j++;
+		}
+		cout<<i<<" and "<<count <<"\n";
+		char_count[k++]=count;
+		i=i+count;
+		count=1;
+	}
+
+	for(i=0;i<k;i++){
+		cout<<char_count[i]<<"\n";
+	}
+	
+	i=0;
+	j=0;
+	while(i<len){
+	    count=char_count[j++];
+	    cout<<string[i]<<count;
+	    i=i+count;
+	}
+}
