7ede864c30e7e51820d0b47d0d3923a6ca1def9d
Kaavyasri-Srinivasan <kaavyasri.069@gmail.com>
2026-06-16 14:47:51 +0530

    Refactor string mismatch identification logic

--- string_mismatch_identification.cpp
+++ string_mismatch_identification.cpp
@@ -1,65 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    string st1;
-    string st2;
-    cout<<"Enter string 1: ";
-    cin>>st1;
-    cout<<"Enter string 2: ";
-    cin>>st2;
-    int len1=st1.length();
-    int len2=st2.length();
-    vector <char> new_elt;
-    vector <char> new_elt2;
-    map <string,string> mpp;
-    set <string>res;
-    
-    int count=0;
-    if (len1!=len2){
-        cout<<"String size should be same:";
+    vector <char>new_elt1;
+    vector <char>new_elt2;
+    vector <pair<string, string>> v;
+    string s1,s2;
+    cout<<"Enter string:";
+    cin>>s1;
+    cin>>s2;
+    int len1=s1.length();
+    int len2=s2.length();
+    int j;
+    if(len1!=len2){
+        cout<<"Length should be equal";
         return 0;
     }
-    
-    else{
-        for(int i=0;i<len1; i++){
-            if(st1[i]!=st2[i]){
-                new_elt.push_back(st1[i]);
-                new_elt2.push_back(st2[i]);
+    for(int i=0;i<len1; i++){
+        j=i;
+        
+        if(s1[i] != s2[i]){
+            while(j<len1 && s1[j] != s2[j]){
+                new_elt1.push_back(s1[j]);
+                new_elt2.push_back(s2[j]);
+                j++;
                 
             }
-            if(st1[i]==st2[i]){
-                string result(new_elt.begin(), new_elt.end());
-                string result2(new_elt2.begin(), new_elt2.end());
-                res.insert(result);
-                res.insert(result2);
-                cout<<result<<" ";
-                cout<<result2<<" ";
-                    cout<<"\n";
-                mpp[result]=result2;
-                result.clear();
-                result2.clear();
-                
-            }
+            string res1(new_elt1.begin(), new_elt1.end());
+            string res2(new_elt2.begin(), new_elt2.end());
+            v.push_back({res1, res2});
+            new_elt1.clear();
+            new_elt2.clear();
             
+            i=j-1;
         }
-        
-        
+     
+        else{
+            continue;
+        }
     }
-    string result(new_elt.begin(), new_elt.end());
-    string result2(new_elt2.begin(), new_elt2.end());
-    res.insert(result);
-    res.insert(result2);
-    cout<<result<<" ";
-    cout<<result2<<" ";
-    cout<<"\n";
-    mpp[result]=result2;
-    result.clear();
-    result2.clear();
-    for(auto it : res){
-        cout<<it<< " ";
-    }
-    cout<<"MAP \n";
-    for(auto it: mpp){
-        cout<< it.first<< "-->"<<it.second<<"\n";
+    for(auto it: v){
+        cout<<it.first<<"-->"<<it.second<<"\n";
     }
 }
