0fafac917ffbc39c6738c77496dd9972f73a5eef
Kaavyasri-Srinivasan <kaavyasri.069@gmail.com>
2026-06-16 14:05:13 +0530

    Improve pattern.cpp by cleaning up code
    Refactor pattern generation logic and remove unused variables.

--- pattern.cpp
+++ pattern.cpp
@@ -4,36 +4,30 @@ int main(){
     int n;
     cout<<"Enter number of rows: ";
     cin>>n;
-    int ans;
-    int iter;
-    int l;
+    
     int space, star;
     int j;
+    int res;
+    int temp;
+   
     for(int i=0; i<n; i++){
         space=n -(i+1);
         for(int s=0;s<space;s++){
-            cout<<" ";
+            cout<<"\t";
         }
-        star=n-space;
-        for(int st=0; st<star;st++){
-            iter=st+1;
-            ans=iter;
-            j=st;
-            
-            while (j>0){
-                // cout<<"Inside while loop of "<< st << "\n";
-                ans=iter;
-                
-                for(int val=st; val >0; val--){
-                    l=j;
-                    ans=ans+(n-l); 
-                    l--;
-                }
-                cout<<ans;
-                j--;
-            }
-            cout<<iter;
+        if(i==0){
+            res=1;
+            cout<<res;
+            goto loop_start;
         }
+        res=res+(n-i+1);
+        cout<<res<<"\t";
+        temp=res;
+        for(j=i; j>0; j--){
+            temp=temp-(n-j);
+            cout<<temp<<"\t";
+        }
+        loop_start:
         cout<<"\n";
     }
 }
